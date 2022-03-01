bdc6d7b3185c36514f78c23caac8a8b6cd1c9b21
indigobluw <lindquist20@gmail.com>
2022-03-01 09:51:48 +0100

    pics and try to connect to frontend

--- app.js
+++ app.js
@@ -15,6 +15,47 @@ app.get("/", (req ,res)=>{
   res.status(200).send(body)
 })
 
+app.get("/game", (req, res)=>{
+  headers = { http_status: 200, "cache-control": "no-cache" };
+  body = [
+    {
+      "imgSrc": "./image/chikorita.png",
+      "name": "chikorita"
+  },
+  { 
+      "imgSrc": "./image/dragonite.png",
+      "name": "dragonite"
+  },
+  {
+      "imgSrc":"./image/eevee.png",
+      "name": "eevee"
+  },
+  {
+      "imgSrc":"./image/groudon.png",
+      "name": "groudon"
+  },
+  {
+      "imgSrc":"./image/gyrados.png",
+      "name": "gyrados"
+  },
+  {
+      "imgSrc":"./image/lugia.png",
+      "name": "lugia"
+  },
+  {
+      "imgSrc":"./image/metagross.png",
+      "name": "metagross"
+  },
+  {
+      "imgSrc":"./image/milotic.png",
+      "name": "milotic"
+  }  
+  ];
+
+  res.set("Content-Type", "application/json");
+  res.status(200).send(body);
+});
+
 app.listen(PORT , ()=>{
   console.log('STARTED LISTENING ON PORT ${PORT}')
 });
