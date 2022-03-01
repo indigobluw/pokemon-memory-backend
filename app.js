0d663a62ef6d02ce203ad375327018e0618d52c5
indigobluw <lindquist20@gmail.com>
2022-03-01 10:54:04 +0100

    yes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require("cors")
 const express = require("express")
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
 app.use('/healthcheck', require('./routes/healthcheck.routes'));
 app.use(express.urlencoded({ extended: true }));
